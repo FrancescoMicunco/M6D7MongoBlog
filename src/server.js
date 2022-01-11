2c4ec54cc83d3e5d9b445848184529e9773d049c
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-11 14:05:54 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import listEndpoints from 'express-list-endpoints'
+import cors from 'cors'
+import { Mongoose } from 'mongoose'
+
+const server = express()
+
+server.use = cors()
