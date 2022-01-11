8cd6de0d990ec6c2b259da211c28e70bfbc70649
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-11 14:54:33 +0100

    completed serve connection

--- src/server.js
+++ src/server.js
@@ -6,13 +6,32 @@ import BlogPostRouter from './Blogs/schema.js'
 
 const server = express()
 
+const PORT = process.env.PORT || 3001
+
 server.use(cors())
 
 // ==========  endPoints ==========
 server.use('/blogPost', BlogPostRouter)
 
 
+// ==========  errors handlers =========
+
+server.use(badRequestHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
+
 // ========== connection ==========
 
-console.table(listEndpoints(server))
-mongoose.connect(DB_CONNECTION);
+
+mongoose.connect(process.env.DB_CONNECTION);
+
+mongoose.connection.on("server connected", () => {
+    console.log("Mongo server connected")
+
+    server.listen(PORT, () => {
+        console.table(listEndpoints(server))
+        console.log(`server is running on port n. ${PORT}`)
+    })
+})
+
+mongoose.connection.on("error", err => { console.log(err) })
