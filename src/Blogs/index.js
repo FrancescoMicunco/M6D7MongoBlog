cc7f0e6d0b73ed96d8fa1232578615d4df14e136
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-12 15:04:36 +0100

    enpoints finished

--- src/Blogs/index.js
+++ src/Blogs/index.js
@@ -8,7 +8,6 @@ router
     .route('/')
     .post(async(req, res, next) => {
         try {
-            console.log(req.body)
             const blog = await BlogsModel(req.body)
             const { _id } = await blog.save()
             res.status(201).send({ _id })
@@ -40,13 +39,7 @@ router
         }
     })
 
-.post(async(req, res, next) => {
-    try {
 
-    } catch (error) {
-        next(error)
-    }
-})
 
 .put(async(req, res, next) => {
     try {
@@ -73,22 +66,56 @@ router
 
 
 
-
-
 router
     .route('/:id/comments')
     .get(async(req, res, next) => {
         try {
-
+            const blog = await BlogsModel.findById(req.params.id)
+            if (blog) {
+                res.send(blog.comments)
+            } else { res.send("") }
         } catch (error) {
             next(error)
         }
     })
+
+.post(async(req, res, next) => {
+    try {
+        const blogId = await BlogsModel.findById(req.body.blogId, { _id: 0 })
+        console.log("this is blogId", blogId)
+        if (blogId) {
+            const commentToInsert = {...blogId.toObject(), comment: " " }
+            console.log(commentToInsert)
+
+            const updateBlogWithComment = await BlogsModel.findByIdAndUpdate(req.params.blogId, {
+                $push: { comment: commentToInsert }
+            }, { new: true })
+            if (updateBlogWithComment) {
+                res.send(updateBlogWithComment)
+            } else { console.log("there are no blogs to update") }
+
+        }
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
 router
     .route('/:id/comments/:commentId')
     .get(async(req, res, next) => {
         try {
-
+            const blog = await BlogsModel.findById(req.params.id)
+            if (blog) {
+                const comment = blog.comments.find(c => c._id.toString() === req.params.commentId)
+                if (comment) {
+                    res.send(comment)
+                } else { console.log("comment not found!") }
+            } else {
+                console.log("blog not found")
+            }
         } catch (error) {
             next(error)
         }
@@ -96,6 +123,22 @@ router
 
 .put(async(req, res, next) => {
     try {
+        const blog = await BlogsModel.findById(req.params.id)
+        if (blog) {
+
+            const index = blog.comments.findIndex(c => c._id.toString() === req.params.commentId)
+
+            if (index !== -1) {
+
+                blog.comments[index] = {...blog.comments[index].toObject(), ...req.body }
+
+                await blog.save()
+
+            } else {
+                console.log("comment not found!")
+            }
+
+        }
 
     } catch (error) {
         next(error)
@@ -103,13 +146,16 @@ router
 })
 
 .delete(async(req, res, async) => {
-        try {
+    try {
+        const blog = await BlogsModel.findByIdAndUpdate(req.params.id, { $pull: { comments: { _id: req.params.commentId } } }, { new: true })
+        if (blog) {
+            res.send(blog)
+        } else { console.log("comment not found!") }
+    } catch (error) {
+        next(error)
+    }
+})
 
-        } catch (error) {
-            next(error)
-        }
-    })
-    // DELETE / blogPosts /: id / comment /: commentId
 
 
 
