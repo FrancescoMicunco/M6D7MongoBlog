be6c2ea75a0329121450541a0bd98e7d25f26a67
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-02-10 16:10:05 +0100

    token creation

---
+++ src/authentication/token.js
@@ -0,0 +1,21 @@
+import createHttpError from 'http-errors'
+import { verifyJWT, veryfyJWT } from './tools.js'
+
+export const JWTAuthMiddleware = async(req, res, next) => {
+
+    if (!req.headers.authorization) {
+        next(createHttpError(401, "Please provide a token"))
+    } else {
+        try {
+            const token = req.headers.authorization.replace("Bearer", "")
+            const payload = await verifyJWT(token)
+            req.author = {
+                _id: payload._id,
+                role: payload.role,
+            }
+            next()
+        } catch (error) {
+            next(createHttpError(401, "Invalid Token"))
+        }
+    }
+}
